3438d68c977693fcc1ccca8dd4678250ecc244f3
Carlos <thehandyowl.io@gmail.com>
2017-12-28 20:54:25 +0100

    Instalar react-native-router-flux. Configurar 3 vistas básicas. Configurar Router con las 3 vistas. Algo de estilos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux'
 
+// My modules
 import { Colors } from 'MarvelRN/src/commons'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// Views
+import CharactersList from 'MarvelRN/src/sections/characters/CharactersList'
+import CharacterDetail from 'MarvelRN/src/sections/characters/CharacterDetail'
+import CharacterNew from 'MarvelRN/src/sections/characters/CharacterNew'
 
+// Component
 export default class App extends Component {
 
   componentWillMount() {
@@ -30,11 +19,31 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          Marvel RN
-        </Text>
-      </View>
+      <Router>
+        <Scene key='root'>
+          <Scene
+            initial
+            key={ 'CharactersList' }
+            component={ CharactersList }
+            title={ 'Characters List' }
+            navigationBarStyle={ styles.navBar }
+          />
+          <Scene
+            //initial
+            key={ 'CharacterDetail' }
+            component={ CharacterDetail }
+            title={ 'Character Detail' }
+            navigationBarStyle={ styles.navBar }
+          />
+          <Scene
+            //initial
+            key={ 'CharacterNew' }
+            component={ CharacterNew }
+            title={ 'Character New' }
+            navigationBarStyle={ styles.navBar }
+          />
+        </Scene>
+      </Router>
     );
   }
 
@@ -42,11 +51,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: Colors.background,
+  navBar: {
+    backgroundColor: Colors.navBar,
   },
 
 });
