0660bf4cf0bb06f58ded9d42a58c9d31ee50175b
Carlos <thehandyowl.io@gmail.com>
2017-12-28 13:04:36 +0100

    Cambiamos ruta de entrada. Cambiamos estilo a la barra de estado. Añadimos archivo de colores personalizados.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@
 import React, { Component } from 'react';
 import {
   Platform,
+  StatusBar,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+import { Colors } from 'MarvelRN/src/commons'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -19,39 +22,31 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component {
+
+  componentWillMount() {
+    StatusBar.setBarStyle('light-content') // iOS StatusBar light style
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text>
+          Marvel RN
         </Text>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: Colors.background,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
