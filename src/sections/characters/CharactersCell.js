3fd0766ab7619dba0da21ef7ffcb4c8508dafd19
Carlos <thehandyowl.io@gmail.com>
2017-12-31 18:15:41 +0100

    Aplicando estilos a CharactersCell

--- src/sections/characters/CharactersCell.js
+++ src/sections/characters/CharactersCell.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import { Colors } from 'MarvelRN/src/commons'
 
@@ -11,24 +11,72 @@ export default class CharactersCell extends Component {
     }
 
     render () {
-        const item = this.props && this.props.item ? this.props.item : {}
+        const item      = this.props && this.props.item ? this.props.item : {}
+        const name      = item && item.name ? item.name : ''
+        const image     = item && item.thumbnail.path && item.thumbnail.extension ?
+            {
+                uri: item.thumbnail.path.replace('http://', 'https://') +
+                '.' +
+                item.thumbnail.extension
+            } : require('MarvelRN/src/resources/placeholder.png')
+        const comics = item && item.comics.available ?
+                        'Comics: ' + item.comics.available : ''
+        const series = item && item.series.available ?
+                        'Series: ' + item.series.available : ''
+        const stories = item && item.stories.available ?
+                        'Stories: ' + item.stories.available : ''
+
         return (
-            <TouchableOpacity style={{
-                        height: 100,
-                        backgroundColor: Colors.colorSecundario,
-                        marginVertical: 10
-                    }}
+            <TouchableOpacity
                 onPress={ () => this.props.onSelectItem(item) }
             >
-                <Text>{'Index:'}.- {item.id}</Text>
-                <Text>{'Name'}.- {item.name}</Text>
-                <Text>{'Thumbnail'}.- {
-                        item.thumbnail.path + '.' + item.thumbnail.extension
-                    }
-                </Text>
+                <Image
+                    style   = { styles.image }
+                    source  = { image }
+                />
+                <View style = { styles.textContainer }>
+                    <Text style={styles.textBox}>
+                        {name}
+                        {'\n'}
+                        {comics ? comics : ''}
+                        {comics && series ? ' - ' : ''}
+                        {series ? series + '' : ''}
+                        {(comics || series) && stories ? ' - ' : ''}
+                        {stories ? stories : ''}
+                        {comics || series || stories ? '' : 'No info available'}
+                    </Text>
+                </View>
             </TouchableOpacity>
         )
 
     }
 
 }
+
+const styles = StyleSheet.create({
+
+    image: {
+        width: '100%',
+        height: 200,
+    },
+
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+        backgroundColor: Colors.blackShadowBG,
+    },
+
+    textBox: {
+        flex: 1,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: Colors.white,
+        textAlign: 'center',
+    },
+
+})
